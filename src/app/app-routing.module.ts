1b71ad0bec1a9ce1aa0f12a8a857016616a11706
Geovana Moreira de Oliveira <geovanamoreira907@gmail.com>
2021-08-25 22:31:49 -0300

    md eu perdi a esperança na vida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,33 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+
+  {
+    path: 'space-jam',
+    loadChildren: () => import('./filmes/space-jam/space-jam.module').then(m => m.SpaceJamPageModule)
+  },
+
+  {
+    path: 'luca',
+    loadChildren: () => import('./filmes/luca/luca.module').then(m => m.LucaPageModule)
+  },
+
+  {
+    path: 'esquadrao',
+    loadChildren: () => import('./filmes/esquadrao/esquadrao.module').then(m => m.EsquadraoPageModule)
+  },
+
+  {
+    path: 'invocacao',
+    loadChildren: () => import('./filmes/invocacao/invocacao.module').then(m => m.InvocacaoPageModule)
+  },
+
+  {
+    path: 'cruela',
+    loadChildren: () => import('./filmes/cruela/cruela.module').then(m => m.CruelaPageModule)
   }
+
 ];
 @NgModule({
   imports: [
